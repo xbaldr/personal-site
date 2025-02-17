89e7da434e3179dcc9d9bcf4f46235a5acd17a00
cutterjam <cutterjames108@gmail.com>
2025-02-17 07:26:50 -0500

    Update Welcome.tsx

--- src/components/welcome/Welcome.tsx
+++ src/components/welcome/Welcome.tsx
@@ -1,11 +1,12 @@
 export const Welcome = () => (
     <div className="text-center mb-12">
-      <h2 className="text-3xl text-sky-300 mb-6">WELCOME TO MY SITE!</h2>
-      <p className="mb-4 text-black">I've been waiting for you!</p>
-      <p className="mb-8">
-        Feel free to surf through our company information, our projects, 
-        our products and our wonderful job opportunities. 
-        Enjoy your visit and come again soon!
+      <h2 className="text-2xl text-sky-300 font-['Times_New_Roman'] font-bold uppercase">Welcome to My Site!</h2>
+      <p className="mb-4 text-slate-600 font-['Times_New_Roman']">I've been waiting for you!</p>
+      <p className="mb-8 text-slate-600 font-['Times_New_Roman']">
+        This infrastructure of imagination is under active development. 
+        What emerges here will challenge the boundaries of possibility, 
+        transform collective potential, and compose new ways of being
+        together. 
       </p>
     </div>
   );
